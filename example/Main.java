b950543266c4d6c7abb3090d4a31e9f6e8aa1eab
DisolvingCG <119822817+DisolvingCG@users.noreply.github.com>
2023-05-07 16:35:51 -0600

    Delete Main.java

--- example/Main.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-         VentanaSwing ventanaSwing = new VentanaSwing("Centigrados a Farenheid");
-    }
-}
