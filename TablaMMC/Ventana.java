84066ef13fe011008293caf474d46a94b49b4052
DisolvingCG <119822817+DisolvingCG@users.noreply.github.com>
2023-05-12 19:35:11 -0600

    Add files via upload

---
+++ TablaMMC/Ventana.java
@@ -0,0 +1,103 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.jar.JarEntry;
+
+public class Ventana extends JFrame{
+    private JLabel lblId;
+    private JLabel lblNombre;
+    private JLabel lblGenero;
+    private JLabel lblPeso;
+    private JLabel lblPlataforma;
+    private JLabel lblDesarrollador;
+    private JTextField txtId;
+    private JTextField txtNombre;
+    private JTextField txtGenero;
+    private JTextField txtPeso;
+    private JTextField txtPlataforma;
+    private JTextField txtDesarrollador;
+    private JButton btnAgregar;
+    private GridLayout layout;
+    private JPanel panel1;
+    private JPanel panel2;
+    private ModeloTablaJuego modelo;
+    private ArrayList <Videojuego> info;
+    private JScrollPane scroll;
+    private JTable tbltabla;
+
+    public Ventana(String title) throws HeadlessException {
+        super(title);
+        this.setSize(500, 700);
+        layout  = new GridLayout(2, 1);
+        this.setLayout(layout);
+        //panel 1
+        panel1 = new JPanel(new FlowLayout());
+        panel1.setBackground(new Color(71, 101, 143));
+        lblId = new JLabel("Id: ");
+        txtId = new JTextField(4);
+        panel1.add(lblId);
+        panel1.add(txtId);
+        lblNombre = new JLabel("Nombre: ");
+        txtNombre = new JTextField(30);
+        panel1.add(lblNombre);
+        panel1.add(txtNombre);
+        lblGenero = new JLabel("Genero: ");
+        txtGenero = new JTextField(15);
+        panel1.add(lblGenero);
+        panel1.add(txtGenero);
+        lblPeso = new JLabel("Peso en Gb: ");
+        txtPeso = new JTextField(5);
+        panel1.add(lblPeso);
+        panel1.add(txtPeso);
+        lblPlataforma = new JLabel("Plataforma: ");
+        txtPlataforma = new JTextField(15);
+        panel1.add(lblPlataforma);
+        panel1.add(txtPlataforma);
+        lblDesarrollador = new JLabel("Desarrollador: ");
+        txtDesarrollador = new JTextField(25);
+        panel1.add(lblDesarrollador);
+        panel1.add(txtDesarrollador);
+        btnAgregar = new JButton("Agregar juego");
+        panel1.add(btnAgregar);
+
+        this.getContentPane().add(panel1, 0);
+
+        //panel 2
+        panel2 = new JPanel(new FlowLayout());
+        panel2.setBackground(new Color(85, 38, 128));
+        info = new ArrayList<>();
+        info.add(new Videojuego(1, "League of Legends", "MOBA",
+                4.5, "Windows y Mac", "Riot games"));
+        modelo = new ModeloTablaJuego(info);
+        tbltabla = new JTable(modelo);
+        scroll = new JScrollPane(tbltabla);
+        panel2.add(scroll);
+
+
+        this.getContentPane().add(panel2,1);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.btnAgregar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Videojuego nuevo = new Videojuego();
+                nuevo.setId(Integer.parseInt(txtId.getText()));
+                nuevo.setNombre(txtNombre.getText());
+                nuevo.setGenero(txtGenero.getText());
+                nuevo.setPesoEnGb(Double.parseDouble(txtPeso.getText()));
+                nuevo.setPlataforma(txtPlataforma.getText());
+                nuevo.setDesarrollador(txtDesarrollador.getText());
+                //info.add(nuevo);
+                modelo.agregarJuego(nuevo);
+                tbltabla.updateUI();
+            }
+        });
+
+        this.setVisible(true);
+
+    }
+}
